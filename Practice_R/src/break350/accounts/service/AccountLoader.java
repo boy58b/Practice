025f3ac11ca75c631ab37bbe7f817c58443fc679
boy58b <ispas.vova@mail.ru>
2015-06-17 09:53:22 +0300

    Revert "it`s work!"
    This reverts commit fade9d60977c379a51d4a8dbafdd4c9c08b4dd00.

--- Practice_R/src/break350/accounts/service/AccountLoader.java
+++
@@ -1,8 +0,0 @@
-package break350.accounts.service;
-
-import break350.accounts.model.Account;
-import javafx.collections.ObservableList;
-
-public interface AccountLoader {
-	public ObservableList<Account> load();
-}
