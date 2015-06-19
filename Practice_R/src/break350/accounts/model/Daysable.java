4e92c32497b06f52d5d87c4041369dc6c1bcefa5
boy58b <ispas.vova@mail.ru>
2015-06-19 12:31:08 +0300

    Revert "Revert "Revert "it`s work!"""
    This reverts commit bcde8514fcf826a5de4e6147fc5ff923ea6a5adb.

--- Practice_R/src/break350/accounts/model/Daysable.java
+++
@@ -1,5 +0,0 @@
-package break350.accounts.model;
-
-public interface Daysable {
-	public void setWorkingDays(int workingDays);
-}
--- Practice_R/src/break350/accounts/service/AccountLoaderFactory.java
+++
@@ -1,7 +0,0 @@
-package break350.accounts.service;
-
-public class AccountLoaderFactory {
-	public static AccountLoader getAccountLoader() {
-		return new XLSAccountLoader();
-	}
-}
